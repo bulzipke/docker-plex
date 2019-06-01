7175ba7ee3289d5cc8c399e11575eecb9995d844
bulzipke <bulzipke@naver.com>
2019-06-01 22:43:21 +0900

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,7 +2,7 @@
 
 cd /config/Library/Application\ Support/Plex\ Media\ Server/Plug-ins/
 
-curl -L -o DaumMovie.bundle.zip https://github.com/axfree/DaumMovie.bundle/archive/master.zip
+curl -L -o DaumMovie.bundle.zip https://github.com/soju6jan/SJ_Daum.bundle/archive/master.zip
 curl -L -o NaverMusic.bundle.zip https://github.com/SOULITY/NaverMusic.bundle/archive/master.zip
 curl -L -o AlsongLyricFind.bundle.zip https://github.com/soju6jan/AlsongLyricFind.bundle/archive/master.zip
 
