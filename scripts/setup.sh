5370ae4919e496682b7e785989fbcd1d8eb79a5a
bulzipke <bulzipke@naver.com>
2019-06-19 00:34:06 +0900

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,7 +1,5 @@
 #! /bin/bash
 
-sqlite3 -header -line "/config/Library/Application Support/Plex Media Server/Plug-in Support/Databases/com.plexapp.plugins.library.db" "PRAGMA default_cache_size = 1000000"
-
 curl -L -o DaumMovie.bundle.zip https://github.com/soju6jan/SJ_Daum.bundle/archive/master.zip
 curl -L -o NaverMusic.bundle.zip https://github.com/SOULITY/NaverMusic.bundle/archive/master.zip
 curl -L -o AlsongLyricFind.bundle.zip https://github.com/soju6jan/AlsongLyricFind.bundle/archive/master.zip
@@ -10,5 +8,3 @@ ls *.zip | xargs -n1 unzip -q -o
 rename "s/\-master//" *
 rm -rf *.zip
 cp -Rf * /config/Library/Application\ Support/Plex\ Media\ Server/Plug-ins/
-
-
